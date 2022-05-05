c8bfec2f877cb27e81702f860535d416aa7f03f9
Andrey Mnatsakanov <andrey@3ig.kiev.ua>
2022-05-05 13:29:43 +0300

    correct example

--- examples/dnfile.rs
+++ examples/dnfile.rs
@@ -2,9 +2,7 @@
 pub fn main() -> dnfile::Result<()>{
     for (i, arg) in std::env::args().enumerate() {
         if i == 1 {
-            let path = std::path::Path::new(arg.as_str());
-            let buffer = std::fs::read(path)?;
-            let df = dnfile::DnPe::new(arg.as_str(), &buffer)?;
+            let df = dnfile::DnPe::new(arg.as_str())?;
             println!("{:#02x?}", df);
         }
     }
