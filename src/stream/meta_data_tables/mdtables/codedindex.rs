f607b2ad06f4f0b66c2d7268c12aac774b9ffaa2
Sriram Govindan <marirs@gmail.com>
2022-05-22 17:37:56 +0530

    refactor

--- src/stream/meta_data_tables/mdtables/codedindex.rs
+++ src/stream/meta_data_tables/mdtables/codedindex.rs
@@ -37,7 +37,7 @@ pub trait CodedIndex {
         let value = crate::utils::read_usize(value)?;
         let table_name = self.get_table_name(value & ((1 << self.get_tag_bits()) - 1))?;
         self.set_row_index(value >> self.get_tag_bits());
-        for (_, t) in tables {
+        for t in tables.values() {
             if t.table.name() != table_name {
                 continue;
             }
